66092d19b45dad8694c5bee194542acebe833d7d
Ryaanajambeg <ryaan.ajambeg@students.plymouth.ac.uk>
2021-11-18 17:41:21 +0000

    All pages done

---
+++ app/src/main/java/com/example/comp2000cw1ex4/cancelbookingconfirmActivity.java
@@ -0,0 +1,14 @@
+package com.example.comp2000cw1ex4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cancelbookingconfirmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cancelbookingconfirm);
+    }
+}
---
+++ app/src/main/java/com/example/comp2000cw1ex4/managebookingActivity.java
@@ -0,0 +1,14 @@
+package com.example.comp2000cw1ex4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class managebookingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_managebooking);
+    }
+}
